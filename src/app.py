1b6322b456e3bd45bc5ca2b06013656935e58761
Alvaro Sena <alvarosenacs.c@gmail.com>
2022-04-02 14:33:26 -0300

    Project setup.

---
+++ src/app.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+from models import db
+from flask_migrate import Migrate 
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+db.init_app(app)
+migrate = Migrate(app, db)
+
+@app.route('/')
+def index():
+    return {"message": "Hello, World"}
+
---
+++ src/models.py
@@ -0,0 +1,42 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+
+db = SQLAlchemy()
+
+class Client(db.Model):
+    __tablename__ = 'clients'
+
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    created_at = db.Column(db.DateTime(timezone=True), default=func.now())
+
+class Buyer(db.Model):
+    __tablename__ = 'buyers'
+
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(180), unique=True, nullable=False)
+    cpf = db.Column(db.String(11), unique=True, nullable=False)
+    payments = db.relationship('Payment', backref='payment')
+    created_at = db.Column(db.DateTime(timezone=True), default=func.now())
+
+class Card(db.Model):
+    __tablename__ = 'cards'
+
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    holder_name = db.Column(db.String, nullable=False)
+    card_number = db.Column(db.String, nullable=False)
+    expiration_date = db.Column(db.DateTime, nullable=False)
+    cvv = db.Column(db.Integer, unique=True, nullable=False)
+    payments = db.relationship('Payment', backref='payment')
+    created_at = db.Column(db.DateTime(timezone=True), default=func.now())
+
+class Payment(db.Model):
+    __tablename__ = 'payments'
+
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    amount = db.Column(db.Float, nullable=False)
+    type = db.Column(db.String, nullable=False)
+    buyer_id = db.Column(db.Integer, db.ForeignKey('buyers.id'))
+    card_id = db.Column(db.Integer, db.ForeignKey('cards.id'))
+    created_at = db.Column(db.DateTime(timezone=True), default=func.now())
+    
